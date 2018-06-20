83f5478b6a97dde6f7c110f07f6a4bf7bd64fab2
aqi00 <aqi00@163.com>
2018-06-20 11:14:25 +0800

    《Kotlin从零到精通Android开发》服务端源码

---
+++ HttpTest/src/servlet/CheckUpdate.java
@@ -0,0 +1,66 @@
+package servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CheckUpdate extends HttpServlet {
+
+	public CheckUpdate() {
+		super();
+	}
+
+	public void init() throws ServletException {
+	}
+
+	public void destroy() {
+		super.destroy();
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("begin doPost");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				request.getInputStream()));
+		StringBuilder tempStr = new StringBuilder();
+		while (reader.read() != -1) {
+			tempStr.append(reader.readLine());
+		}
+		String request_str = tempStr.toString();
+		System.out.println("request_str=" + request_str);
+
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+
+		String response_str = getJsonStr();
+		System.out.println("response_str=" + response_str);
+		try {
+			out.println(response_str);
+		} catch (Exception e) {
+			System.out.println("e.getMessage()=" + e.getMessage());
+			out.println(e.getMessage());
+		}
+
+		out.flush();
+		out.close();
+	}
+
+	private String getJsonStr() {
+		String str = "{\"app_name\":\"爱奇艺\",\"package_name\":\"com.qiyi.video\",\"version_code\":80930,\"version_name\":\"8.8.5\",\"need_update\":true,\"download_url\":\"http://www.lenovomm.com/appdown/21404091-2\"}";
+		return str;
+	}
+
+}
